9a2de5dbb27a097f9d0709db02b88f6b907ea045
Dmitrii Bruslik <dmitrii.bruslik@ignited.it>
2020-07-22 11:13:56 +0300

    Union ZeroFlzt and Flat

--- src/components/Flat/index.tsx
+++ src/components/Flat/index.tsx
@@ -1,2 +1,20 @@
-export { default as Flat } from './Flat';
-export { default as ZeroFlat } from './ZeroFlat';
+import React, { FC } from 'react';
+import './styles.scss';
+import String from '../String';
+
+interface IFlat {
+  id: number;
+  width: number;
+  config: any[];
+  zero?: boolean;
+}
+
+const Flat = ({ id, width, config, zero = false }) => {
+  return (
+    <div className={`flat ${zero && 'zero'}`} style={zero ? undefined : { width }}>
+      {config.map((string, num) => (<String key={`${zero ? 0 : id}F+${num}S`} {...string} />))}
+    </div>
+  );
+};
+
+export default Flat;
