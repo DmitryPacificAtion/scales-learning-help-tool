9f7e4af70756c158690de33bbff4a6667ba61530
DmitryPacificAtion <bruslik.dmitry@gmail.com>
2020-08-04 16:28:44 +0300

    Add reminder

--- src/components/Fingerboard/index.tsx
+++ src/components/Fingerboard/index.tsx
@@ -4,6 +4,7 @@ import Flat from '../Flat';
 import { TOTAL_FRETS, CHROMATIC_SCALE } from '../../constants/global';
 
 const Fingerboard = () => {
+     // TODO: Лады вместить в струны, а не наоборот
     const flats = new Array(TOTAL_FRETS);
     for (var i = 0, width = 76; i <= TOTAL_FRETS; i++, width--) {
         flats.push(<Flat key={i + 'F'} id={i} width={width} zero={i === 0} config={CHROMATIC_SCALE} />);
